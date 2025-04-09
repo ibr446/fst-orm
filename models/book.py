fd190123fc3b2b0799e2f58e51ac332c18420d84
Lenovo <bunyodibratov@gmail.com>
2025-04-09 09:56:21 +0500

    Orm

--- models/book.py
+++ models/book.py
@@ -4,7 +4,7 @@ from tortoise.models import Model
 
 class Book(Model):
     id = fields.IntField(pk=True)
-    author = fields.ForeignKeyField('models.User', related_name="books")
+    author = fields.ForeignKeyField('models.User', on_delete=fields.CASCADE, related_name="books")
     title = fields.CharField(max_length=150)
     description = fields.TextField()
     created_at = fields.DatetimeField(auto_now_add=True)
